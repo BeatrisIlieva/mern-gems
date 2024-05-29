9888d9fcf77496142476e8d5cd1b9be8ce02757c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-05-29 16:24:23 +0300

    Added test

--- client/src/components/User/Register/RegisterForm/RegisterForm.test.js
+++ client/src/components/User/Register/RegisterForm/RegisterForm.test.js
@@ -11,6 +11,182 @@ const mockAuthContextValue = {
 };
 
 describe("RegisterFrom Component", () => {
+  test("Fill First Name with valid data expect success", async () => {
+    render(
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <RegisterForm />
+      </AuthContext.Provider>
+    );
+
+    const button = screen.getByTestId("submit");
+    expect(button).toBeInTheDocument();
+
+    const input = screen.getByTestId(`${FORM_KEYS.FirstName}-input`);
+    expect(input).toBeInTheDocument();
+
+    fireEvent.change(input, { target: { value: "Test" } });
+    fireEvent.click(button);
+
+    const errorMessageContainer = screen.getByTestId(
+      `${FORM_KEYS.FirstName}-error`
+    );
+    const errorMessageContent = errorMessageContainer.textContent.trim();
+    expect(errorMessageContent).toBe("");
+  });
+});
+
+describe("RegisterFrom Component", () => {
+  test("Fill First Name with empty string expect error", async () => {
+    render(
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <RegisterForm />
+      </AuthContext.Provider>
+    );
+
+    const button = screen.getByTestId("submit");
+    expect(button).toBeInTheDocument();
+
+    const firstNameInput = screen.getByTestId(`${FORM_KEYS.FirstName}-input`);
+
+    fireEvent.change(firstNameInput, { target: { value: "" } });
+    fireEvent.click(button);
+
+    const firstNameErrorMessageContainer = screen.getByTestId(
+      `${FORM_KEYS.FirstName}-error`
+    );
+    const firstNamErrorMessageContent =
+      firstNameErrorMessageContainer.textContent.trim();
+    expect(firstNamErrorMessageContent).toBe(ERROR_MESSAGES.firstName);
+  });
+});
+
+describe("RegisterFrom Component", () => {
+  test("Fill First Name with a single letter expect error", async () => {
+    render(
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <RegisterForm />
+      </AuthContext.Provider>
+    );
+
+    const button = screen.getByTestId("submit");
+    expect(button).toBeInTheDocument();
+
+    const firstNameInput = screen.getByTestId(`${FORM_KEYS.FirstName}-input`);
+
+    fireEvent.change(firstNameInput, { target: { value: "T" } });
+    fireEvent.click(button);
+
+    const firstNameErrorMessageContainer = screen.getByTestId(
+      `${FORM_KEYS.FirstName}-error`
+    );
+    const firstNamErrorMessageContent =
+      firstNameErrorMessageContainer.textContent.trim();
+    expect(firstNamErrorMessageContent).toBe(ERROR_MESSAGES.firstName);
+  });
+});
+
+describe("RegisterFrom Component", () => {
+  test("Fill First Name including digit expect error", async () => {
+    render(
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <RegisterForm />
+      </AuthContext.Provider>
+    );
+
+    const button = screen.getByTestId("submit");
+    expect(button).toBeInTheDocument();
+
+    const firstNameInput = screen.getByTestId(`${FORM_KEYS.FirstName}-input`);
+
+    fireEvent.change(firstNameInput, { target: { value: "T1" } });
+    fireEvent.click(button);
+
+    const firstNameErrorMessageContainer = screen.getByTestId(
+      `${FORM_KEYS.FirstName}-error`
+    );
+    const firstNamErrorMessageContent =
+      firstNameErrorMessageContainer.textContent.trim();
+    expect(firstNamErrorMessageContent).toBe(ERROR_MESSAGES.firstName);
+  });
+});
+
+describe("RegisterFrom Component", () => {
+  test("Fill Last Name with valid data expect success", async () => {
+    render(
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <RegisterForm />
+      </AuthContext.Provider>
+    );
+
+    const button = screen.getByTestId("submit");
+    expect(button).toBeInTheDocument();
+
+    const lastNameInput = screen.getByTestId(`${FORM_KEYS.LastName}-input`);
+    expect(lastNameInput).toBeInTheDocument();
+
+    fireEvent.change(lastNameInput, { target: { value: "Test" } });
+    fireEvent.click(button);
+
+    const lastNameErrorMessageContainer = screen.getByTestId(
+      `${FORM_KEYS.LastName}-error`
+    );
+    const lastNameErrorMessageContent =
+      lastNameErrorMessageContainer.textContent.trim();
+    expect(lastNameErrorMessageContent).toBe("");
+  });
+});
+
+describe("RegisterFrom Component", () => {
+  test("Fill Last Name with empty string expect error", async () => {
+    render(
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <RegisterForm />
+      </AuthContext.Provider>
+    );
+
+    const button = screen.getByTestId("submit");
+    expect(button).toBeInTheDocument();
+
+    const lastNameInput = screen.getByTestId(`${FORM_KEYS.LastName}-input`);
+
+    fireEvent.change(lastNameInput, { target: { value: "" } });
+    fireEvent.click(button);
+
+    const lastNameErrorMessageContainer = screen.getByTestId(
+      `${FORM_KEYS.LastName}-error`
+    );
+    const lastNameErrorMessageContent =
+      lastNameErrorMessageContainer.textContent.trim();
+    expect(lastNameErrorMessageContent).toBe(ERROR_MESSAGES.lastName);
+  });
+});
+
+describe("RegisterFrom Component", () => {
+  test("Fill Last Name with a single letter expect error", async () => {
+    render(
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <RegisterForm />
+      </AuthContext.Provider>
+    );
+
+    const button = screen.getByTestId("submit");
+    expect(button).toBeInTheDocument();
+
+    const firstNameInput = screen.getByTestId(`${FORM_KEYS.FirstName}-input`);
+
+    fireEvent.change(firstNameInput, { target: { value: "T" } });
+    fireEvent.click(button);
+
+    const firstNameErrorMessageContainer = screen.getByTestId(
+      `${FORM_KEYS.FirstName}-error`
+    );
+    const firstNamErrorMessageContent =
+      firstNameErrorMessageContainer.textContent.trim();
+    expect(firstNamErrorMessageContent).toBe(ERROR_MESSAGES.firstName);
+  });
+});
+
+describe("RegisterFrom Component", () => {
   test("Should successfully fill the form with valid input", async () => {
     render(
       <AuthContext.Provider value={mockAuthContextValue}>
