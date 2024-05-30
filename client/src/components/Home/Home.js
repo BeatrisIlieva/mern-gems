b7e0f13a39cfeb59abbf1c84a0006fccf47e9dc2
Beatris Ilieve <beatrisilieve@icloud.com>
2024-05-30 16:20:48 +0300

    Added home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,82 @@
 import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+import { VerticalLine } from "../VerticalLine/VerticalLine";
 
 export const Home = () => {
-
-    return (
-        <section className={styles["home-box"]}>
-            <p>home</p>
-        </section>
-    )
-}
+  return (
+   
+    <section className={styles["hero"]}>
+      <Link to="/">
+        <div className={styles["hero-box"]}>
+          <div className={styles["hero-text"]}>
+            <p>text</p>
+          </div>
+          <img
+            className={styles["hero-img"]}
+            src={
+              "https://res.cloudinary.com/deztgvefu/image/upload/v1716995569/collections/forgetmenot_rz0umv.png"
+            }
+            alt={"Img"}
+          />
+        </div>
+      </Link>
+      <VerticalLine/>
+      <Link to="/">
+        <div className={styles["hero-box-reverse"]}>
+          <div className={styles["hero-text"]}>
+            <p>text</p>
+          </div>
+          <img
+            className={styles["hero-img"]}
+            src={
+              "https://res.cloudinary.com/deztgvefu/image/upload/v1716995568/collections/pirouette_zshews.webp"
+            }
+            alt={"Img"}
+          />
+        </div>
+      </Link>
+      <Link to="/">
+        <div className={styles["hero-box"]}>
+          <div className={styles["hero-text"]}>
+            <p>text</p>
+          </div>
+          <img
+            className={styles["hero-img"]}
+            src={
+              "https://res.cloudinary.com/deztgvefu/image/upload/v1716995568/collections/diamondloop_hzmacd.webp"
+            }
+            alt={"Img"}
+          />
+        </div>
+      </Link>
+      <Link to="/">
+        <div className={styles["hero-box-reverse"]}>
+          <div className={styles["hero-text"]}>
+            <p>text</p>
+          </div>
+          <img
+            className={styles["hero-img"]}
+            src={
+              "https://res.cloudinary.com/deztgvefu/image/upload/v1716995568/collections/sunflower_jreu5p.webp"
+            }
+            alt={"Img"}
+          />
+        </div>
+      </Link>
+      <Link to="/">
+        <div className={styles["hero-box"]}>
+          <div className={styles["hero-text"]}>
+            <p>text</p>
+          </div>
+          <img
+            className={styles["hero-img"]}
+            src={
+              "https://res.cloudinary.com/deztgvefu/image/upload/v1716995569/collections/sparklingcluster_hpovka.webp"
+            }
+            alt={"Img"}
+          />
+        </div>
+      </Link>
+    </section>
+  );
+};
