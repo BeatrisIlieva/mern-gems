57b7874afd02e72b9075241623d0c3882d92532b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-05-31 19:02:32 +0300

    Added test

--- client/src/components/User/Login/LoginForm/LoginForm.test.js
+++ client/src/components/User/Login/LoginForm/LoginForm.test.js
@@ -11,30 +11,59 @@ const mockAuthContextValue = {
 };
 
 describe("LoginFrom Component", () => {
-    test("Fill Credentials with invalid email expect error", async () => {
-      render(
-        <AuthContext.Provider value={mockAuthContextValue}>
-          <LoginForm />
-        </AuthContext.Provider>
-      );
-  
-      const button = screen.getByTestId("submit");
-      expect(button).toBeInTheDocument();
-  
-      const emailInput = screen.getByTestId(`${FORM_KEYS.Email}-input`);
-      expect(emailInput).toBeInTheDocument();
-      fireEvent.change(emailInput, { target: { value: "t@l.c" } });
+  test("Fill Credentials with invalid email expect error", async () => {
+    render(
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <LoginForm />
+      </AuthContext.Provider>
+    );
 
-      const passwordInput = screen.getByTestId(`${FORM_KEYS.Email}-input`);
-      expect(passwordInput).toBeInTheDocument();
-      fireEvent.change(passwordInput, { target: { value: "123456Tt" } });
-  
-      fireEvent.click(button);
-  
-      const errorMessageContainer = screen.getByTestId(
-        `${FORM_KEYS.Email}-error`
-      );
-      const errorMessageContent = errorMessageContainer.textContent.trim();
-      expect(errorMessageContent).toBe(ERROR_MESSAGES.email);
-    });
+    const button = screen.getByTestId("submit");
+    expect(button).toBeInTheDocument();
+
+    const emailInput = screen.getByTestId(`${FORM_KEYS.Email}-input`);
+    expect(emailInput).toBeInTheDocument();
+    fireEvent.change(emailInput, { target: { value: "t@l.c" } });
+
+    const passwordInput = screen.getByTestId(`${FORM_KEYS.Email}-input`);
+    expect(passwordInput).toBeInTheDocument();
+    fireEvent.change(passwordInput, { target: { value: "123456Tt" } });
+
+    fireEvent.click(button);
+
+    const errorMessageContainer = screen.getByTestId(
+      `${FORM_KEYS.Email}-error`
+    );
+    const errorMessageContent = errorMessageContainer.textContent.trim();
+    expect(errorMessageContent).toBe(ERROR_MESSAGES.email);
   });
+});
+
+describe("LoginFrom Component", () => {
+  test("Fill Credentials with invalid password expect error", async () => {
+    render(
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <LoginForm />
+      </AuthContext.Provider>
+    );
+
+    const button = screen.getByTestId("submit");
+    expect(button).toBeInTheDocument();
+
+    const emailInput = screen.getByTestId(`${FORM_KEYS.Email}-input`);
+    expect(emailInput).toBeInTheDocument();
+    fireEvent.change(emailInput, { target: { value: "test@email.com" } });
+
+    const passwordInput = screen.getByTestId(`${FORM_KEYS.Email}-input`);
+    expect(passwordInput).toBeInTheDocument();
+    fireEvent.change(passwordInput, { target: { value: "123456" } });
+
+    fireEvent.click(button);
+
+    const errorMessageContainer = screen.getByTestId(
+      `${FORM_KEYS.Password}-error`
+    );
+    const errorMessageContent = errorMessageContainer.textContent.trim();
+    expect(errorMessageContent).toBe(ERROR_MESSAGES.password);
+  });
+});
