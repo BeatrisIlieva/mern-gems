7c386b9c343dbe312da461add2f8b94299aba93c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-01 11:23:04 +0300

    Added test

--- client/src/components/User/Login/Login.test.js
+++ client/src/components/User/Login/Login.test.js
@@ -11,7 +11,7 @@ const mockAuthContextValue = {
   onRegisterSubmit: mockOnRegisterSubmit,
 };
 
-describe("Register Component", () => {
+describe("Login Component", () => {
   test("Should load image", async () => {
     render(
       <AuthContext.Provider value={mockAuthContextValue}>
@@ -26,71 +26,17 @@ describe("Register Component", () => {
   });
 });
 
-// describe("Register Component", () => {
-//   test("Should load title", async () => {
-//     render(
-//       <AuthContext.Provider value={mockAuthContextValue}>
-//         <BrowserRouter>
-//           <Register />
-//         </BrowserRouter>
-//       </AuthContext.Provider>
-//     );
-
-//     const titleElement = screen.getByTestId("title-element");
-//     expect(titleElement).toBeInTheDocument();
-//   });
-// });
-
-// describe("Register Component", () => {
-//   test("Should load sub-title", async () => {
-//     render(
-//       <AuthContext.Provider value={mockAuthContextValue}>
-//         <BrowserRouter>
-//           <Register />
-//         </BrowserRouter>
-//       </AuthContext.Provider>
-//     );
-
-//     const subTitleElement = screen.getByTestId("sub-title-element");
-//     expect(subTitleElement).toBeInTheDocument();
-//   });
-// });
-
-// describe("Register Component", () => {
-//   test("Should load password requirements", async () => {
-//     render(
-//       <AuthContext.Provider value={mockAuthContextValue}>
-//         <BrowserRouter>
-//           <Register />
-//         </BrowserRouter>
-//       </AuthContext.Provider>
-//     );
-
-//     const passwordRequirementsElement = screen.getByTestId(
-//       "password-requirements-element"
-//     );
-//     expect(passwordRequirementsElement).toBeInTheDocument();
-//   });
-// });
-
-// describe("Register Component", () => {
-//   test("Should load password requirements", async () => {
-//     render(
-//       <AuthContext.Provider value={mockAuthContextValue}>
-//         <MemoryRouter initialEntries={["/user/register"]}>
-//           <Routes>
-//             <Route path="/user/login" element={<Login />} />
-//             <Route path="/user/register" element={<Register />} />
-//           </Routes>
-//         </MemoryRouter>
-//       </AuthContext.Provider>
-//     );
-
-//     const signInButton = screen.getByTestId("sign-in-button");
-//     expect(signInButton).toBeInTheDocument();
-//     fireEvent.click(signInButton);
-
-//     const signInTitleElement = screen.getByTestId("sign-in-title-element");
-//     expect(signInTitleElement).toBeInTheDocument();
-//   });
-// });
+describe("Login Component", () => {
+    test("Should load sing in title", async () => {
+      render(
+        <AuthContext.Provider value={mockAuthContextValue}>
+          <BrowserRouter>
+            <Login />
+          </BrowserRouter>
+        </AuthContext.Provider>
+      );
+  
+      const singInTitleElement = screen.getByTestId("sign-in-title-element");
+      expect(singInTitleElement).toBeInTheDocument();
+    });
+  });
