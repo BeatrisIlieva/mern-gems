ad8dc7d8864509a3eae6a3cdac6f1e6bc10a0b03
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-01 18:07:35 +0300

    Added user account funtionality

---
+++ client/src/services/addressInformationService.js
@@ -0,0 +1,13 @@
+import { requestFactory } from "./requester";
+import { HOST } from "../constants/host";
+
+const baseUrl = `${HOST}/user-address-information`;
+
+export const personalInformationServiceFactory = (token) => {
+  const request = requestFactory(token);
+
+  return {
+    find: (userId) => request.get(`${baseUrl}/${userId}`),
+    update: (userId, data) => request.put(`${baseUrl}/${userId}`, data),
+  };
+};
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const userLoginInformationController = require("./controllers/userLoginInformationController");
 const userPersonalInformationController = require("./controllers/userPersonalInformationController");
+const userAddressInformationController = require("./controllers/userAddressInformationController");
 
 router.use("/user-login-information", userLoginInformationController);
 router.use("/user-personal-information", userPersonalInformationController);
+router.use("/user-address-information", userAddressInformationController);
 
 module.exports = router;
