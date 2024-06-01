dd049a0c01d21450b79a693ae9ecf2fd86430859
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-01 17:35:08 +0300

    Added user account funtionality

--- client/src/components/User/Account/PersonalInformation/AddressInformationForm/AddressInformationForm.js
+++ client/src/components/User/Account/PersonalInformation/AddressInformationForm/AddressInformationForm.js
@@ -1,14 +1,41 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import styles from "./AddressInformationForm.module.css";
+import { AddressInformationFormPopup } from "./AddressInformationFormPopup/AddressInformationFormPopup";
 
 export const AddressInformationForm = () => {
+  const [
+    displayAddressInformationFormPopup,
+    setDisplayAddressInformationFormPopup,
+  ] = useState(false);
+
+  const clickHandler = async () => {
+    document.body.style.overflow = "hidden";
+    setDisplayAddressInformationFormPopup(true);
+  };
+
+  const submitHandler = () => {
+    document.body.style.overflow = "visible";
+    setDisplayAddressInformationFormPopup(false);
+  };
+
+  const closeHandler = () => {
+    document.body.style.overflow = "visible";
+    setDisplayAddressInformationFormPopup(false);
+  };
+
   return (
     <section className={styles["address-container"]}>
-      <button className={styles["button"]}>
+      <button className={styles["button"]} onClick={clickHandler}>
         <FontAwesomeIcon icon={faCirclePlus} className={styles["icon"]} />
         <span>Add a New Address</span>
       </button>
+      {displayAddressInformationFormPopup && (
+        <AddressInformationFormPopup
+          submitHandler={submitHandler}
+          closeHandler={closeHandler}
+        />
+      )}
     </section>
   );
 };
