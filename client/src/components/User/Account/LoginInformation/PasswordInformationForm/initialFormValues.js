4ff261c7205211e9a08620061ccecba03cc6e08a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-02 12:45:15 +0300

    Updated user account

---
+++ client/src/components/User/Account/LoginInformation/PasswordInformationForm/initialFormValues.js
@@ -0,0 +1,33 @@
+import { PASSWORD_PATTERN } from "../../../../../constants/forms";
+
+const FORM_KEYS = {
+  Password: "password",
+  NewPassword: "newPassword",
+  RetypeNewPassword: "retypeNewPassword",
+};
+
+export const INITIAL_FORM_VALUES = {
+  [FORM_KEYS.Password]: {
+    fieldLabel: "Current Password",
+    fieldValue: "",
+    regexPattern: PASSWORD_PATTERN,
+    errorMessage: "",
+    isFocused: false,
+  },
+  [FORM_KEYS.NewPassword]: {
+    fieldLabel: "New Password",
+    fieldValue: "",
+    regexPattern: PASSWORD_PATTERN,
+    errorMessage: "",
+    isFocused: false,
+  },
+  [FORM_KEYS.RetypeNewPassword]: {
+    fieldLabel: "Confirm Password",
+    fieldValue: "",
+    regexPattern: PASSWORD_PATTERN,
+    errorMessage: "",
+    isFocused: false,
+  },
+};
+
+export { FORM_KEYS };
