09022fa1f3990264df08bd922c32813ceb582f87
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-02 17:37:07 +0300

    Updated user account

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "normalize.css";
-// import "./commonCSS/font-sizes.module.css";
-// import "./commonCSS/font-families.module.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
