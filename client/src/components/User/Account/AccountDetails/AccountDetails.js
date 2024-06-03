0aab251ffbbc8417ad30930f1803f6d806132262
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-03 13:44:16 +0300

    Updated user account

--- client/src/components/User/Account/AccountDetails/AccountDetails.js
+++ client/src/components/User/Account/AccountDetails/AccountDetails.js
@@ -20,29 +20,35 @@ export const AccountDetails = () => {
     setShowUpdateEmail(false);
   };
   return (
-    <section className={styles["personal-information-box"]}>
+    <section className={styles["account-details-box"]}>
       <div className={styles["left-container"]}>
         <h2 className={styles["form-title"]}>Personal Information</h2>
         <PersonalInformationForm />
       </div>
       <div className={styles["right-container"]}>
         <div className={styles["right-top-container"]}>
-          <h2 className={styles["form-title-address"]}>Address Book</h2>
-          <AddressInformationForm />
-        </div>
-        <div className={styles["right-bottom-container"]}>
-          <button
+          <h2 className={styles["form-title"]}>Login Information</h2>
+          <h4 className={styles["form-sub-title"]}>Email Address</h4>
+<div className={styles["button-container"]}>
+<button
+            className={styles["button"]}
             onClick={() => onUpdateEmailClick()}
           >
             Update Email Address
           </button>
           <button
+            className={styles["button"]}
             onClick={() => onUpdatePasswordClick()}
           >
             Change Password
           </button>
+</div>
           {showUpdateEmail && <EmailInformationForm />}
           {showUpdatePassword && <PasswordInformationForm />}
+        </div>
+        <div className={styles["right-bottom-container"]}>
+          <h2 className={styles["form-title-address"]}>Address Book</h2>
+          <AddressInformationForm />
         </div>
       </div>
     </section>
