783b4979a8030188268ec257cc3a58f4af41a8b2
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-06 18:13:18 +0300

    Added test

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -25,26 +25,3 @@ export const useLocalStorage = (key, initialValue) => {
 
   return [state, setState];
 };
-
-// import { useState } from "react";
-
-// export const useLocalStorage = (key, initialValue) => {
-//   const [state, setState] = useState(() => {
-//     const persistedStatesSerialized = localStorage.getItem(key);
-//     if (persistedStatesSerialized) {
-//       const persistedState = JSON.parse(persistedStatesSerialized);
-
-//       return persistedState;
-//     }
-
-//     return initialValue;
-//   });
-
-//   const setLocalStorageState = (value) => {
-//     setState(value);
-
-//     localStorage.setItem(key, JSON.stringify(value));
-//   };
-
-//   return [state, setLocalStorageState];
-// };
