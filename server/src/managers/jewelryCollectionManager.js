4e82452aa1214bc37b1a9020d4b9974a04f6b2b5
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-11 16:36:00 +0300

    Updated jewelry functionality

--- server/src/managers/jewelryCollectionManager.js
+++ server/src/managers/jewelryCollectionManager.js
@@ -130,14 +130,7 @@ exports.findAll = async (data) => {
         isLikedByUser: 1,
       },
     },
-    { $sort: { isSoldOut: 1, _id: 1 }},
-    // {
-    //   $sort: {
-    //     _id: 1,
-    //   },
-    // },
-    // { $skip: data.skip },
-    // { $limit: data.limit },
+    { $sort: { isSoldOut: 1, _id: 1 } },
   ];
 
   const countQuery = [
