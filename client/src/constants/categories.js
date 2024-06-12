206f8fc4afd9c592882a04b5247bc7dc48ae6e8b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-12 12:08:54 +0300

    Updated jewelry functionality

--- client/src/constants/categories.js
+++ client/src/constants/categories.js
@@ -1,17 +1,3 @@
-export const CATEGORIES_BY_NAMES = {
-  bracelets: 1,
-  earrings: 2,
-  necklaces: 3,
-  rings: 4,
-};
-
-export const CATEGORIES_BY_IDS = {
-  1: "Bracelets",
-  2: "Earrings",
-  3: "Necklaces & Pendants",
-  4: "Rings",
-};
-
 export const CATEGORIES_BY_ID_AND_TITLE = {
   Bracelets: [1, "Bracelets"],
   Earrings: [2, "Earrings"],
