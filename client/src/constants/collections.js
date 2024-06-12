d1b669cf1d3f7bd6cf7fbd16a4f26c4e397614c5
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-12 12:11:04 +0300

    Updated jewelry functionality

--- client/src/constants/collections.js
+++ client/src/constants/collections.js
@@ -1,21 +1,3 @@
-export const COLLECTIONS_BY_NAMES = {
-  "diamond-loop": 1,
-  sunflower: 2,
-  "sparkling-cluster": 3,
-  "forget-me-not": 4,
-  pirouette: 5,
-  classics: 6,
-};
-
-export const COLLECTIONS_BY_IDS = {
-  1: "Diamond Loop",
-  2: "Sunflower",
-  3: "Sparkling Cluster",
-  4: "Forget-Me-Not",
-  5: "Pirouette",
-  6: "Classics",
-};
-
 export const CATEGORIES_BY_ID_AND_TITLE = {
   "Diamond Loop": [1, "Diamond Loop"],
   Sunflower: [2, "Sunflower"],
