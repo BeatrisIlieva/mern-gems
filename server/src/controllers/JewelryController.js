a46082ebc077fd0e8d2e40dc597dd01671b982cb
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-13 14:28:08 +0300

    Updated jewelry functionality

--- server/src/controllers/JewelryController.js
+++ server/src/controllers/JewelryController.js
@@ -38,63 +38,6 @@ router.get("/by-category/:categoryId", async (req, res) => {
   }
 });
 
-// router.get("/by-stone-types", async (req, res) => {
-
-//   const serializedObject =
-//     req.query.data !== "undefined" ? req.query.data : "undefined";
-//   const jewelryDictionary =
-//     req.query.data !== "undefined"
-//       ? JSON.parse(decodeURIComponent(serializedObject))
-//       : null;
-
-//       const jewelryIds = jewelryDictionary["JewelryIds"];
-//       console.log(jewelryIds)
-
-//   data = { jewelryIds };
-
-//   try {
-
-//     const stoneTypesData = await getStoneTypesData(jewelryIds);
-
-//     result = { stoneTypesData };
-
-//     res.status(200).json(result);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(401).json({
-//       message: err.message,
-//     });
-//   }
-// });
-
-// router.get("/by-stone-colors", async (req, res) => {
-
-//   const serializedObject =
-//     req.query.data !== "undefined" ? req.query.data : "undefined";
-//   const jewelryDictionary =
-//     req.query.data !== "undefined"
-//       ? JSON.parse(decodeURIComponent(serializedObject))
-//       : null;
-
-//       const jewelryIds = jewelryDictionary["JewelryIds"];
-
-//   data = { jewelryIds };
-
-//   try {
-
-//     const stoneColorsData = await getStoneColorsData(jewelryIds);
-
-//     result = { stoneColorsData };
-
-//     res.status(200).json(result);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(401).json({
-//       message: err.message,
-//     });
-//   }
-// });
-
 router.get("/by-jewelry/:jewelryId", async (req, res) => {
   try {
     const jewelryId = Number(req.params.jewelryId);
