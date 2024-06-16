92440afd51fb872d1609e553c9aed5c2d2b07b7a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-16 17:30:47 +0300

    Updated bag functionality

--- client/src/components/Bag/Bag.js
+++ client/src/components/Bag/Bag.js
@@ -97,12 +97,12 @@ export const Bag = () => {
           {loading && <LoadingSpinner />}
         </div>
       ) : (
-        <>
+        <section section className={styles["bag-box"]}>
           <div className={styles["bag-sub-title"]}>
             <h3>Your Shopping Bag is Empty.</h3>
             <p>Explore and add something you love.</p>
           </div>
-        </>
+        </section>
       )}
     </section>
   );
