e907cf597bcb8290a227ddfe81b63bdec1bd9a6c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-17 20:11:55 +0300

    Updated complete order funtionality

--- client/src/components/MonthDropdown/MonthDropdown.js
+++ client/src/components/MonthDropdown/MonthDropdown.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import styles from "./MonthDropdown.module.css";
 
 export const MonthDropdown = () => {
   const [months, setMonths] = useState([]);
-  const [selectedMonth, setSelectedMonth] = useState('MM');
+  const [selectedMonth, setSelectedMonth] = useState("MM");
 
   useEffect(() => {
     const generateMonthOptions = () => {
-      const currentMonth = new Date().getMonth() + 1; 
+      const currentMonth = new Date().getMonth() + 1;
       const monthsArray = [];
 
       for (let month = currentMonth; month <= 12; month++) {
-        monthsArray.push(month.toString().padStart(2, '0')); /
+        monthsArray.push(month.toString().padStart(2, "0"));
       }
 
       setMonths(monthsArray);
@@ -20,14 +21,13 @@ export const MonthDropdown = () => {
   }, []);
 
   return (
-    <select value={selectedMonth} onChange={(e) => setSelectedMonth(e.target.value)}>
-      <option value="MM" disabled>MM</option>
+    <select className={styles["select-dropdown"]} value={selectedMonth} onChange={(e) => setSelectedMonth(e.target.value)}>
+      <option  value="MM" disabled>MM</option>
       {months.map((month) => (
         <option key={month} value={month}>
-          {month}
+         <span className={styles["select-option"]}>{month}</span>
         </option>
       ))}
     </select>
   );
 };
-
