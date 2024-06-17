77c6c3a3e5d0adc1de8364069691c1e6b6d82b0d
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-17 19:30:12 +0300

    Updated complete order funtionality

--- client/src/components/DayDropdown/DayDropdown.js
+++ client/src/components/DayDropdown/DayDropdown.js
@@ -2,22 +2,21 @@ import React, { useState, useEffect } from "react";
 
 export const DayDropdown = () => {
   const [days, setDays] = useState([]);
+  const [selectedDay, setSelectedDay] = useState("DD");
 
   useEffect(() => {
     const generateDayOptions = () => {
       const currentDate = new Date();
       const currentDay = currentDate.getDate();
-      const currentMonth = currentDate.getMonth() + 1; // Months are 0-indexed in JavaScript
+      const currentMonth = currentDate.getMonth() + 1;
       const currentYear = currentDate.getFullYear();
-
-      // Function to get the number of days in the current month
       const daysInMonth = (month, year) => new Date(year, month, 0).getDate();
 
       const totalDays = daysInMonth(currentMonth, currentYear);
       const daysArray = [];
 
       for (let day = currentDay; day <= totalDays; day++) {
-        daysArray.push(day.toString().padStart(2, "0")); // Add leading zero if needed
+        daysArray.push(day.toString().padStart(2, "0"));
       }
 
       setDays(daysArray);
@@ -27,7 +26,13 @@ export const DayDropdown = () => {
   }, []);
 
   return (
-    <select>
+    <select
+      value={selectedDay}
+      onChange={(e) => setSelectedDay(e.target.value)}
+    >
+      <option value="DD" disabled>
+        DD
+      </option>
       {days.map((day) => (
         <option key={day} value={day}>
           {day}
--- client/src/components/MonthDropdown/MonthDropdown.js
+++ client/src/components/MonthDropdown/MonthDropdown.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 export const MonthDropdown = () => {
   const [months, setMonths] = useState([]);
+  const [selectedMonth, setSelectedMonth] = useState('MM');
 
   useEffect(() => {
     const generateMonthOptions = () => {
-      const currentMonth = new Date().getMonth() + 1; // Months are 0-indexed in JavaScript
+      const currentMonth = new Date().getMonth() + 1; 
       const monthsArray = [];
 
       for (let month = currentMonth; month <= 12; month++) {
-        monthsArray.push(month.toString().padStart(2, "0")); // Add leading zero if needed
+        monthsArray.push(month.toString().padStart(2, '0')); /
       }
 
       setMonths(monthsArray);
@@ -19,7 +20,8 @@ export const MonthDropdown = () => {
   }, []);
 
   return (
-    <select>
+    <select value={selectedMonth} onChange={(e) => setSelectedMonth(e.target.value)}>
+      <option value="MM" disabled>MM</option>
       {months.map((month) => (
         <option key={month} value={month}>
           {month}
@@ -28,3 +30,4 @@ export const MonthDropdown = () => {
     </select>
   );
 };
+
