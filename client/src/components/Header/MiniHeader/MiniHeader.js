81d27619118454c7ef1e35aa3c091d712de3e2de
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-17 11:23:53 +0300

    Updated Header

--- client/src/components/Header/MiniHeader/MiniHeader.js
+++ client/src/components/Header/MiniHeader/MiniHeader.js
@@ -5,17 +5,15 @@ export const MiniHeader = () => {
   return (
     <header className={styles["mini-header"]}>
       <div className={styles["header-box"]}>
-        <Link to="/">
-          <div className={styles["logo-container"]}>
-            <img
-              className={styles["logo-img"]}
-              src={
-                "https://res.cloudinary.com/deztgvefu/image/upload/v1714938396/template_images/Screenshot_2024-05-05_at_22.42.20-removebg-preview_xfkrvq.png"
-              }
-              alt={"Logo"}
-            />
-          </div>
-        </Link>
+        <div className={styles["logo-container"]}>
+          <img
+            className={styles["logo-img"]}
+            src={
+              "https://res.cloudinary.com/deztgvefu/image/upload/v1714938396/template_images/Screenshot_2024-05-05_at_22.42.20-removebg-preview_xfkrvq.png"
+            }
+            alt={"Logo"}
+          />
+        </div>
       </div>
     </header>
   );
