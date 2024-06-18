8776831d9666d2bc88c116cbc556f75bc08dc140
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-18 10:26:42 +0300

    Updated complete order functionality

--- client/src/components/Payment/CardDetailsForm/CardDetailsForm.js
+++ client/src/components/Payment/CardDetailsForm/CardDetailsForm.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useService } from "../../../hooks/useService";
 import { useAuthContext } from "../../../contexts/AuthContext";
 import { YearDropdown } from "./YearDropdown/YearDropdown";
+import { MonthDropdown } from "./MonthDropdown/MonthDropdown";
 
 export const CardDetailsForm = () => {
   const { userId } = useAuthContext();
@@ -11,6 +12,7 @@ export const CardDetailsForm = () => {
   return (
     <section className={styles["card-details-box"]}>
 <YearDropdown/>
+<MonthDropdown/>
     </section>
   );
 };
--- client/src/components/Payment/CardDetailsForm/MonthDropdown/MonthDropdown.js
+++ client/src/components/Payment/CardDetailsForm/MonthDropdown/MonthDropdown.js
@@ -1,30 +1,29 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "../CardDetailsForm.module.css";
 
-export const YearDropdown = () => {
-  const [years, setYears] = useState([]);
-  const [selectedYear, setSelectedYear] = useState("YY");
+export const MonthDropdown = () => {
+  const [months, setMonths] = useState([]);
+  const [selectedMonth, setSelectedMonth] = useState("YY");
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
 
-  useEffect(() => {
-    const generateYearOptions = () => {
-      const currentYear = new Date().getFullYear();
-      const endYear = currentYear + 10;
-      const yearsArray = [];
+    useEffect(() => {
+    const generateMonthOptions = () => {
+      const currentMonth = new Date().getMonth() + 1;
+      const monthsArray = [];
 
-      for (let year = currentYear + 1; year <= endYear; year++) {
-        yearsArray.push(year.toString());
+      for (let month = currentMonth; month <= 12; month++) {
+        monthsArray.push(month.toString().padStart(2, "0"));
       }
 
-      setYears(yearsArray);
+      setMonths(monthsArray);
     };
 
-    generateYearOptions();
+    generateMonthOptions();
   }, []);
 
-  const handleSelect = (year) => {
-    setSelectedYear(year);
+  const handleSelect = (month) => {
+    setSelectedMonth(month);
     setIsOpen(false);
   };
 
@@ -52,14 +51,14 @@ export const YearDropdown = () => {
         className={styles["dropdown-toggle"]}
         onClick={() => setIsOpen(!isOpen)}
       >
-        {selectedYear}
+        {selectedMonth}
       </button>
       {isOpen && (
         <ul className={styles["dropdown-menu"]}>
           <li onClick={() => handleSelect("DD")}>YY</li>
-          {years.map((year) => (
-            <li key={year} onClick={() => handleSelect(year)}>
-              {year}
+          {months.map((month) => (
+            <li key={month} onClick={() => handleSelect(month)}>
+              {month}
             </li>
           ))}
         </ul>
