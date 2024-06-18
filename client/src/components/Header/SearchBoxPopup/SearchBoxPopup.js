1e8f7569771e9589170a06bc2698597e2d90e8b5
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-18 19:00:23 +0300

    Updated complete order functionality

--- client/src/components/Header/SearchBoxPopup/SearchBoxPopup.js
+++ client/src/components/Header/SearchBoxPopup/SearchBoxPopup.js
@@ -87,105 +87,124 @@ export const SearchBoxPopup = ({ popupCloseHandler }) => {
               ))}
             </div>
           ) : (
-            <div className={styles["suggestions-container"]}>
-              <div className={styles["collections-container"]}>
-                <h3>Collections</h3>
-                <Link to="/forget-me-not" className={styles["no-decoration"]}>
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="forget-me-not-title"
-                  >
-                    Forget-Me-Not Collection
-                  </h2>
-                </Link>
-                <Link to="/classics" className={styles["no-decoration"]}>
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="classics-title"
-                  >
-                    Classics Collection
-                  </h2>
-                </Link>
-                <Link to="/pirouette" className={styles["no-decoration"]}>
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="pirouette-title"
-                  >
-                    Pirouette Collection
-                  </h2>
-                </Link>
-                <Link to="/diamond-loop" className={styles["no-decoration"]}>
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="diamond-loop-title"
-                  >
-                    Diamond Loop Collection
-                  </h2>
-                </Link>
-                <Link to="/sunflower" className={styles["no-decoration"]}>
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="sunflower-title"
-                  >
-                    Sunflower Collection
-                  </h2>
-                </Link>
-                <Link
-                  to="/sparkling-cluster"
-                  className={styles["no-decoration"]}
-                >
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="sparkling-cluster-title"
-                  >
-                    Sparkling Cluster Collection
-                  </h2>
-                </Link>
+            <div className={styles["suggestions-box"]}>
+              <div className={styles["suggestions-container"]}>
+                <div className={styles["left-container"]}>
+                  <h3 className={styles["suggestions-title"]}>Collections</h3>
+                  <div className={styles["collections-container"]}>
+                    <Link
+                      to="/forget-me-not"
+                      className={styles["no-decoration"]}
+                    >
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="forget-me-not-title"
+                      >
+                        Forget-Me-Not Collection
+                      </h2>
+                    </Link>
+                    <Link to="/classics" className={styles["no-decoration"]}>
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="classics-title"
+                      >
+                        Classics Collection
+                      </h2>
+                    </Link>
+                    <Link to="/pirouette" className={styles["no-decoration"]}>
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="pirouette-title"
+                      >
+                        Pirouette Collection
+                      </h2>
+                    </Link>
+                    <Link
+                      to="/diamond-loop"
+                      className={styles["no-decoration"]}
+                    >
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="diamond-loop-title"
+                      >
+                        Diamond Loop Collection
+                      </h2>
+                    </Link>
+                    <Link to="/sunflower" className={styles["no-decoration"]}>
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="sunflower-title"
+                      >
+                        Sunflower Collection
+                      </h2>
+                    </Link>
+                    <Link
+                      to="/sparkling-cluster"
+                      className={styles["no-decoration"]}
+                    >
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="sparkling-cluster-title"
+                      >
+                        Sparkling Cluster Collection
+                      </h2>
+                    </Link>
+                  </div>
+                </div>
+                <div className={styles["right-container"]}>
+                  <h3 className={styles["suggestions-title"]}>Categories</h3>
+                  <div className={styles["collections-container"]}>
+                    <Link to="/bracelets" className={styles["no-decoration"]}>
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="forget-me-not-title"
+                      >
+                        Bracelets
+                      </h2>
+                    </Link>
+                    <Link to="/earrings" className={styles["no-decoration"]}>
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="classics-title"
+                      >
+                        Earrings
+                      </h2>
+                    </Link>
+                    <Link to="/necklaces" className={styles["no-decoration"]}>
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="pirouette-title"
+                      >
+                        Necklaces
+                      </h2>
+                    </Link>
+                    <Link to="/rings" className={styles["no-decoration"]}>
+                      <h2
+                        onClick={() => popupCloseHandler()}
+                        className={styles["collection-title"]}
+                        data-testid="diamond-loop-title"
+                      >
+                        Rings
+                      </h2>
+                    </Link>
+                  </div>
+                </div>
               </div>
-              <div className={styles["collections-container"]}>
-                <h3>Categories</h3>
-                <Link to="/bracelets" className={styles["no-decoration"]}>
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="forget-me-not-title"
-                  >
-                    Bracelets
-                  </h2>
-                </Link>
-                <Link to="/earrings" className={styles["no-decoration"]}>
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="classics-title"
-                  >
-                    Earrings
-                  </h2>
-                </Link>
-                <Link to="/necklaces" className={styles["no-decoration"]}>
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="pirouette-title"
-                  >
-                    Necklaces
-                  </h2>
-                </Link>
-                <Link to="/rings" className={styles["no-decoration"]}>
-                  <h2
-                    onClick={() => popupCloseHandler()}
-                    className={styles["collection-title"]}
-                    data-testid="diamond-loop-title"
-                  >
-                    Rings
-                  </h2>
-                </Link>
+              <div className={styles["image-container"]}>
+                <img
+                  className={styles["image"]}
+                  src="https://res.cloudinary.com/deztgvefu/image/upload/v1718723117/template_images/herolarged_ny24_plp_ny_collection_znuyt7.avif"
+                  alt="img"
+                />
               </div>
             </div>
           )}
