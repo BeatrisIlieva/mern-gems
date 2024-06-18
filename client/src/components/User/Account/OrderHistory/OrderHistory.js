98a7780c34e3a5ef3146c6a94564f2d89743cf2f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-18 19:10:03 +0300

    Updated complete order functionality

--- client/src/components/User/Account/OrderHistory/OrderHistory.js
+++ client/src/components/User/Account/OrderHistory/OrderHistory.js
@@ -1,1 +1,10 @@
-export const OrderHistory = () => {};
+import { addressInformationServiceFactory } from "../../../../services/addressInformationService";
+import { useService } from "../../../../hooks/useService";
+import { useAuthContext } from "../../../../contexts/AuthContext";
+import { useState, useEffect } from "react";
+
+export const OrderHistory = () => {
+  const addressInformationService = useService(
+    addressInformationServiceFactory
+  );
+};
