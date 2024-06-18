272a10a71517824ab4984fa94074161dca511e1a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-18 10:50:36 +0300

    Updated complete order functionality

--- client/src/components/Payment/CardDetailsForm/initialFormValues.js
+++ client/src/components/Payment/CardDetailsForm/initialFormValues.js
@@ -1,16 +1,37 @@
 import {
   LONG_CARD_NUMBER_PATTERN,
   CARD_HOLDER_NAME_PATTERN,
-  EXPIRATION_DATE_PATTERN,
-  CVV_CODE_PATTERN_ERROR_MESSAGE,
 } from "../../../../../constants/forms";
 
-
 const FORM_KEYS = {
-    LongCardNumber: "longCardNumber",
-    CardHolder: "cardHolder",
-    Month: "city",
-    Street: "street",
-    Apartment: "apartment",
-    ZipCode: "zipCode",
-  };
+  LongCardNumber: "longCardNumber",
+  CardHolder: "cardHolder",
+};
+
+export const INITIAL_FORM_VALUES = {
+  [FORM_KEYS.LongCardNumber]: {
+    fieldLabel: "Card Number",
+    fieldValue: "",
+    regexPattern: LONG_CARD_NUMBER_PATTERN,
+    errorMessage: "",
+    isFocused: false,
+    fieldType: "text",
+    validTestData: "0123456789123456",
+    invalidTestData: "012345678912345T",
+    emptyTestData: "",
+  },
+
+  [FORM_KEYS.CardHolder]: {
+    fieldLabel: "Name on card",
+    fieldValue: "",
+    regexPattern: CARD_HOLDER_NAME_PATTERN,
+    errorMessage: "",
+    isFocused: false,
+    fieldType: "text",
+    validTestData: "Test",
+    invalidTestData: "Test1",
+    emptyTestData: "",
+  },
+};
+
+export { FORM_KEYS };
