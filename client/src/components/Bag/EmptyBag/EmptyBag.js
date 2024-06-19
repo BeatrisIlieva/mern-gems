a586866d56631d619802e756f3a725d16adcdfd5
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-19 19:49:23 +0300

    Updated Bag functionality

--- client/src/components/Bag/EmptyBag/EmptyBag.js
+++ client/src/components/Bag/EmptyBag/EmptyBag.js
@@ -5,6 +5,7 @@ import { HeroPirouette } from "../../JewelryList/HeroJewelryList/HeroPirouette/H
 import { HeroDiamondLoop } from "../../JewelryList/HeroJewelryList/HeroDiamondLoop/HeroDiamondLoop";
 import { HeroSunflower } from "../../JewelryList/HeroJewelryList/HeroSunflower/HeroSunflower";
 import { HeroSparklingCluster } from "../../JewelryList/HeroJewelryList/HeroSparklingCluster/HeroSparklingCluster";
+import { Link } from "react-router-dom";
 export const EmptyBag = () => {
   return (
     <section className={styles["empty-bag-box"]}>
@@ -12,12 +13,24 @@ export const EmptyBag = () => {
         <h3>Your Shopping Bag is Empty.</h3>
         <p>Explore and add something you love.</p>
       </div>
-      <HeroForgetMeNot />
-      <HeroClassics />
-      <HeroPirouette />
-      <HeroDiamondLoop />
-      <HeroSunflower />
-      <HeroSparklingCluster />
+      <Link to="/forget-me-not" className={styles["no-decoration"]}>
+        <HeroForgetMeNot />
+      </Link>
+      <Link to="/classics" className={styles["no-decoration"]}>
+        <HeroClassics />
+      </Link>
+      <Link to="/pirouette" className={styles["no-decoration"]}>
+        <HeroPirouette />
+      </Link>
+      <Link to="/diamond-loop" className={styles["no-decoration"]}>
+        <HeroDiamondLoop />
+      </Link>
+      <Link to="/sunflower" className={styles["no-decoration"]}>
+        <HeroSunflower />
+      </Link>
+      <Link to="/sparkling-cluster" className={styles["no-decoration"]}>
+        <HeroSparklingCluster />
+      </Link>
     </section>
   );
 };
