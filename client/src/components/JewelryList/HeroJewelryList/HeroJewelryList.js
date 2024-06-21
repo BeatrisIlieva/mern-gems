0b4c76c438f4575a492c967c98899c63000be0d6
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-21 17:49:11 +0300

    Updated hero functionality

--- client/src/components/JewelryList/HeroJewelryList/HeroJewelryList.js
+++ client/src/components/JewelryList/HeroJewelryList/HeroJewelryList.js
@@ -10,40 +10,66 @@ import { HeroPirouette } from "./HeroPirouette/HeroPirouette";
 import { HeroDiamondLoop } from "./HeroDiamondLoop/HeroDiamondLoop";
 import { HeroSunflower } from "./HeroSunflower/HeroSunflower";
 import { HeroSparklingCluster } from "./HeroSparklingCluster/HeroSparklingCluster";
+import { HEROES_BY_TITLE } from "../../../constants/heroes";
+import styles from "./HeroJewelryList.module.css";
+import { HorizontalLine } from "../../HorizontalLine/HorizontalLine";
 
 export const HeroJewelryList = ({ entityTitle }) => {
   return (
-    <>
-      {entityTitle === CATEGORIES_BY_ID_AND_TITLE["Earrings"][1] && (
-        <HeroEarring entityTitle={entityTitle} />
-      )}
-      {entityTitle === CATEGORIES_BY_ID_AND_TITLE["Rings"][1] && (
-        <HeroRing entityTitle={entityTitle} />
-      )}
-      {entityTitle === CATEGORIES_BY_ID_AND_TITLE["Necklaces"][1] && (
-        <HeroNecklace entityTitle={entityTitle} />
-      )}
-      {entityTitle === CATEGORIES_BY_ID_AND_TITLE["Bracelets"][1] && (
-        <HeroBracelet entityTitle={entityTitle} />
-      )}
-      {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Forget-Me-Not"][1] && (
-        <HeroForgetMeNot entityTitle={entityTitle} />
-      )}
-      {/* {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Classics"][1] && (
-        <HeroClassics entityTitle={entityTitle} />
-      )} */}
-      {/* {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Pirouette"][1] && (
-        <HeroPirouette entityTitle={entityTitle} />
-      )} */}
-      {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Diamond Loop"][1] && (
-        <HeroDiamondLoop entityTitle={entityTitle} />
-      )}
-      {/* {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Sunflower"][1] && (
-        <HeroSunflower entityTitle={entityTitle} />
-      )} */}
-      {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Sparkling Cluster"][1] && (
-        <HeroSparklingCluster entityTitle={entityTitle} />
-      )}
-    </>
+    <div className={styles["hero-top-container"]}>
+      <div className={styles["hero-img-container"]}>
+        <img
+          className={styles["hero-img"]}
+          src={HEROES_BY_TITLE[entityTitle][0]}
+          alt={"Img"}
+        />
+      </div>
+      <div className={styles["info-container"]}>
+        <h2 className={styles["box-title"]}>{entityTitle}</h2>
+        <HorizontalLine />
+        <div className={styles["paragraph-container"]}>
+          <p className={styles["box-paragraph"]}>
+            {HEROES_BY_TITLE[entityTitle][1]}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
+
+// export const HeroJewelryList = ({ entityTitle }) => {
+//   return (
+//     <>
+//       {entityTitle === CATEGORIES_BY_ID_AND_TITLE["Earrings"][1] && (
+//         <HeroEarring entityTitle={entityTitle} />
+//       )}
+//       {entityTitle === CATEGORIES_BY_ID_AND_TITLE["Rings"][1] && (
+//         <HeroRing entityTitle={entityTitle} />
+//       )}
+//       {entityTitle === CATEGORIES_BY_ID_AND_TITLE["Necklaces"][1] && (
+//         <HeroNecklace entityTitle={entityTitle} />
+//       )}
+//       {entityTitle === CATEGORIES_BY_ID_AND_TITLE["Bracelets"][1] && (
+//         <HeroBracelet entityTitle={entityTitle} />
+//       )}
+//       {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Forget-Me-Not"][1] && (
+//         <HeroForgetMeNot entityTitle={entityTitle} />
+//       )}
+//       {/* {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Classics"][1] && (
+//         <HeroClassics entityTitle={entityTitle} />
+//       )} */}
+//       {/* {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Pirouette"][1] && (
+//         <HeroPirouette entityTitle={entityTitle} />
+//       )} */}
+//       {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Diamond Loop"][1] && (
+//         <HeroDiamondLoop entityTitle={entityTitle} />
+//       )}
+//       {/* {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Sunflower"][1] && (
+//         <HeroSunflower entityTitle={entityTitle} />
+//       )} */}
+//       {entityTitle === COLLECTIONS_BY_ID_AND_TITLE["Sparkling Cluster"][1] && (
+//         <HeroSparklingCluster entityTitle={entityTitle} />
+//       )}
+//     </>
+//   );
+// };
---
+++ client/src/constants/heroes.js
@@ -0,0 +1,18 @@
+export const HEROES_BY_TITLE = {
+  Bracelets: [
+    "https://res.cloudinary.com/deztgvefu/image/upload/v1718125861/ReactGems/common_img/Screenshot_2024-06-11_at_20.09.57_cc9fga.png",
+    "From sparkling diamond bracelets to chic diamond bangles, explore the brilliant designs from the House of React Gems'.",
+  ],
+  Earrings: [
+    "https://res.cloudinary.com/deztgvefu/image/upload/v1718118512/ReactGems/common_img/largeherod_l2_hj_incre_heartshape_ea_main_rjzvzb.avif",
+    "From floral fine jewelry styles to bold designs, to classic earstuds, chandeliers and diamond drops, explore React Gems' selection of diamond earrings.",
+  ],
+  "Necklaces & Pendants": [
+    "https://res.cloudinary.com/deztgvefu/image/upload/v1718123000/ReactGems/common_img/herolarged_incredibles_yellow_1_w7m1st.avif",
+    "React Gems's precious gemstones necklaces and pendants, set in platinum, offer a more delicate interpretation of the House's signature fine jewelry aesthetic.",
+  ],
+  Rings: [
+    "https://res.cloudinary.com/deztgvefu/image/upload/v1718123003/ReactGems/common_img/largeherod_l2_hj_hope_2_main_ahryjw.avif",
+    "React Gems' rings feature the most exceptional diamonds and gemstones and are known for their unequivocal beauty.",
+  ],
+};
