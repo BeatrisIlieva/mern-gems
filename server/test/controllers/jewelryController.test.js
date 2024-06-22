6955db0bfd08b6c4468ddfa2cb38abf166f75c99
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-22 19:09:06 +0300

    Added test

--- server/test/controllers/jewelryController.test.js
+++ server/test/controllers/jewelryController.test.js
@@ -2,17 +2,6 @@ const supertest = require("supertest");
 const { app, server } = require("../../src/index");
 const request = supertest(app);
 const { connectDB, disconnectDB } = require("../database");
-const UserLoginInformation = require("../../src/models/UserLoginInformation");
-const UserPersonalInformation = require("../../src/models/UserPersonalInformation");
-const UserAddressInformation = require("../../src/models/UserAddressInformation");
-const Bag = require("../../src/models/Bag");
-const {
-  DEFAULT_ADD_QUANTITY,
-  DEFAULT_MIN_QUANTITY,
-  NOT_SELECTED_SIZE_ERROR_MESSAGE,
-  SOLD_OUT_JEWELRY_ERROR_MESSAGE,
-} = require("../../src/constants/bag");
-const Jewelry = require("../../src/models/Jewelry");
 
 describe("bagController", () => {
   beforeAll(async () => {
