b5d541c78017504c0611725153d6a47f037f57b4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-23 13:55:23 +0300

    Added test

--- server/test/controllers/searchController.test.js
+++ server/test/controllers/searchController.test.js
@@ -41,4 +41,23 @@ describe("searchController", () => {
 
     expect(responseBody.length).toBe(4);
   });
+
+  test("Test search a jewelry by stone color and category; Expect four jewelries to be found", async () => {
+    await request.get("/").set("user-uuid", userUUID);
+
+    const search = "pink earring";
+
+    const res = await request
+      .get(`/search/find-all/${userUUID}`)
+      .set("user-uuid", userUUID)
+      .query({ query: search });
+
+    expect(res.status).toBe(200);
+
+    const responseBody = res.body;
+
+    console.log(responseBody);
+
+    expect(responseBody.length).toBe(1);
+  });
 });
