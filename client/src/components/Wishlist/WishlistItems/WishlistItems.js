9a9a1bde5d41ea3741c8cca1800261a1ac42eae5
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-24 15:20:39 +0300

    Added test

--- client/src/components/Wishlist/WishlistItems/WishlistItems.js
+++ client/src/components/Wishlist/WishlistItems/WishlistItems.js
@@ -13,7 +13,7 @@ export const WishlistItems = ({
   mouseEnterHandler,
   mouseLeaveHandler,
   toggleLike,
-  price
+  price,
 }) => {
   const { onRemoveFromWishlistClick } = useWishlistContext();
 
