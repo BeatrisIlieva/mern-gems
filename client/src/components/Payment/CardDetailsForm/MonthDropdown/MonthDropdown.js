c16bbff678dd55dd169d5e654b418a9fb4515f3c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-06-24 10:18:24 +0300

    Added test

--- client/src/components/Payment/CardDetailsForm/MonthDropdown/MonthDropdown.js
+++ client/src/components/Payment/CardDetailsForm/MonthDropdown/MonthDropdown.js
@@ -60,32 +60,43 @@ export const MonthDropdown = ({
       <button
         className={styles["dropdown-toggle"]}
         onClick={() => setIsOpen(!isOpen)}
+        data-testid="dropdown-toggle"
       >
         {selectedMonth}
         {isOpen ? (
           <FontAwesomeIcon
             icon={faChevronUp}
             className={styles["chevron-icon"]}
+            data-testid="chevron-up-icon"
           />
         ) : (
           <FontAwesomeIcon
             icon={faChevronDown}
             className={styles["chevron-icon"]}
+            data-testid="chevron-down-icon"
           />
         )}
       </button>
       {expirationMonthErrorOccurred && (
-        <div className={styles["error-message"]}>
+        <div className={styles["error-message"]} data-testid="error-message">
           {EXPIRATION_MONTH_ERROR_MESSAGE}
         </div>
       )}
       {isOpen && (
-        <ul className={styles["dropdown-menu"]}>
-          <li onClick={() => handleSelect("DD")} className={styles["top-li"]}>
+        <ul className={styles["dropdown-menu"]} data-testid="dropdown-menu">
+          <li
+            onClick={() => handleSelect("DD")}
+            className={styles["top-li"]}
+            data-testid="month-dropdown-title"
+          >
             MM
           </li>
           {months.map((month) => (
-            <li key={month} onClick={() => handleSelect(month)}>
+            <li
+              key={month}
+              onClick={() => handleSelect(month)}
+              data-testid={`month-option-${month}`}
+            >
               {month}
             </li>
           ))}
